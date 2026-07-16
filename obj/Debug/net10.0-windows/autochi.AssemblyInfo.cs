8bf301e5667a82032f57671c94c8d38cccdd5019
jibin0518 <114150533+jibin0518@users.noreply.github.com>
2026-07-16 14:24:45 +0900

    0716

--- obj/Debug/net10.0-windows/autochi.AssemblyInfo.cs
+++ obj/Debug/net10.0-windows/autochi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("autochi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0383fe913b1258ea8fa034415dd92b1fe2fbb253")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+140f025c8d55cecb5bf517d26b6f0be3e4185048")]
 [assembly: System.Reflection.AssemblyProductAttribute("autochi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("autochi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
